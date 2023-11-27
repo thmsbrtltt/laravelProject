0dfdcd348bffcb0c54ee50b005bff9389ab8e12a
thmsbrtltt <w0440892@nscc.ca>
2023-11-27 19:59:20 -0400

    minor commit - removed redundancies

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,3 +1,14 @@
+<style>
+    .form-group {
+        margin-bottom: 20px;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+    }
+</style>
+
 <h2>Create Item</h2>
 
 @if(session('success'))
@@ -9,28 +20,42 @@
 <form action="/items" method="post" enctype="multipart/form-data">
     @csrf
 
-    <label for="category_id">Category:</label>
-    <select name="category_id" id="category_id" required>
-        <!-- Populate with categories if needed -->
-    </select>
+    <div class="form-group">
+        <label for="category_id">Category:</label>
+        <select name="category_id" id="category_id" class="form-control" required>
+           
+        </select>
+    </div>
 
-    <label for="title">Title:</label>
-    <input type="text" name="title" id="title" required>
+    <div class="form-group">
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title" class="form-control" required>
+    </div>
 
-    <label for="description">Description:</label>
-    <textarea name="description" id="description" required></textarea>
+    <div class="form-group">
+        <label for="description">Description:</label>
+        <textarea name="description" id="description" class="form-control" required></textarea>
+    </div>
 
-    <label for="price">Price:</label>
-    <input type="number" name="price" id="price" required>
+    <div class="form-group">
+        <label for="price">Price:</label>
+        <input type="number" name="price" id="price" class="form-control" required>
+    </div>
 
-    <label for="quantity">Quantity:</label>
-    <input type="number" name="quantity" id="quantity" required>
+    <div class="form-group">
+        <label for="quantity">Quantity:</label>
+        <input type="number" name="quantity" id="quantity" class="form-control" required>
+    </div>
 
-    <label for="sku">SKU:</label>
-    <input type="text" name="sku" id="sku" required>
+    <div class="form-group">
+        <label for="sku">SKU:</label>
+        <input type="text" name="sku" id="sku" class="form-control" required>
+    </div>
 
-    <label for="picture">Picture:</label>
-    <input type="file" name="picture" id="picture" required>
+    <div class="form-group">
+        <label for="picture">Picture:</label>
+        <input type="file" name="picture" id="picture" class="form-control" required>
+    </div>
 
-    <button type="submit">Add Item</button>
+    <button type="submit" class="btn btn-primary">Add Item</button>
 </form>
