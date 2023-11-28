f70a2b3fe93ad29ef28c05c4f95d928149d91012
thmsbrtltt <w0440892@nscc.ca>
2023-11-27 22:01:44 -0400

    minor commit

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -20,6 +20,7 @@
 <form action="/items" method="post" enctype="multipart/form-data">
     @csrf
 
+    <!-- Category -->
     <div class="form-group">
         <label for="category_id">Category:</label>
         <select name="category_id" id="category_id" class="form-control" required>
@@ -29,42 +30,45 @@
         </select>
     </div>
 
-    <!-- title -->
+    <!-- Title -->
     <div class="form-group">
         <label for="title">Title:</label>
-        <input type="text" name="title" id="title" class="form-control" required>
+        <input type="text" name="title" id="title" value="{{ old('title') }}" required>
+        @error('title')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <!-- description -->
+    <!-- Description -->
     <div class="form-group">
         <label for="description">Description:</label>
         <textarea name="description" id="description" class="form-control" required></textarea>
     </div>
 
-    <!-- price -->
+    <!-- Price -->
     <div class="form-group">
         <label for="price">Price:</label>
         <input type="number" name="price" id="price" class="form-control" required>
     </div>
 
-    <!-- quantity-->
+    <!-- Quantity -->
     <div class="form-group">
         <label for="quantity">Quantity:</label>
         <input type="number" name="quantity" id="quantity" class="form-control" required>
     </div>
 
-    <!-- sku -->
+    <!-- SKU -->
     <div class="form-group">
         <label for="sku">SKU:</label>
         <input type="text" name="sku" id="sku" class="form-control" required>
     </div>
 
-    <!-- picture -->
+    <!-- Picture -->
     <div class="form-group">
         <label for="picture">Picture:</label>
         <input type="file" name="picture" id="picture" class="form-control" required>
     </div>
 
-    <!-- submit button-->
+    <!-- Submit Button -->
     <button type="submit" class="btn btn-primary">Add Item</button>
 </form>
